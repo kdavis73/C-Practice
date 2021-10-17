4efb340d4be065ed198d665148295ac98221268f
kdavis73 <92394509+kdavis73@users.noreply.github.com>
2021-10-17 01:50:26 -0400

    Create example-2-02.c
    ask the user to enter a number from the keyboard and then print "Hello World" n number of times.

---
+++ Unit 1/example-2-02.c
@@ -0,0 +1,19 @@
+/* In this program, we ask the user to enter a number from the keyboard and 
+then print "Hello World" n number of times. */
+
+#include <stdio.h>
+
+#pragma warning(disable: 4996)
+
+int main() {
+    int i, n;
+    
+    printf("Hi, please enter an integer: ");
+    scanf("%d", &i);
+    
+    for(n = 0; n < i; n++){
+        printf("%d - Hello World!\n", n + 1);
+    }
+    
+    return 0;
+}
